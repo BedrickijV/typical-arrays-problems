d846cf85775d2485aef958e698496a7e14db86e6
bedrickijv <bedrickijv@gmail.com>
2020-03-03 10:34:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array && array.length > 0) {
+        let numb = array[0];
+        array.forEach(function (item, index, array) {
+            if (item <= numb) {
+                return numb = item;
+            }
+
+        });
+        return numb
+    } else {
+        return 0;
+    }
+
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (array && array.length > 0) {
+        let numb = array[0];
+        array.forEach(function (item, index, array) {
+            if (item >= numb) {
+                return numb = item;
+            }
+
+        });
+        return numb
+    } else {
+        return 0;
+    }
+
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (array && array.length > 0) {
+        return array.reduce((a, b) => (a + b)) / array.length;
+    } else {
+        return 0;
+    }
+};
